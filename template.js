c68ca652e93f1d9a511b875f5ff90231772a40eb
ckc2007 <ckc2007@gmail.com>
2023-04-30 14:35:57 -0400

    added shape depend to template

--- template.js
+++ template.js
@@ -1,5 +1,6 @@
-const Circle = 
-
+const Circle = require("./lib/circle");
+const Square = require("./lib/square");
+const Triangle = require("./lib/triangle");
 
 function generateTemplate(data) {
   return ` <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
