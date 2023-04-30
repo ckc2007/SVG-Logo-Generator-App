b2e9457e90c490fea88d7cd4cbea84a36e141892
ckc2007 <ckc2007@gmail.com>
2023-04-30 13:47:53 -0400

    set up index.js with dependencies and questions array

---
+++ index.js
@@ -0,0 +1,8 @@
+// Include the packages needed for this app
+const inquirer = require("inquirer");
+const fs = require("fs");
+// require my template file
+const generateTemplate = require("./template");
+
+// create an array of questions for user input
+const questions = [];
