dde8d2f5ed51c72c6fb4e6c419a71b686bc20a46
ckc2007 <ckc2007@gmail.com>
2023-04-30 14:00:14 -0400

    added questions to index.js

--- index.js
+++ index.js
@@ -5,4 +5,28 @@ const fs = require("fs");
 const generateTemplate = require("./template");
 
 // create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "enter up to three characters for the name:",
+  },
+  {
+    type: "input",
+    name: "text color",
+    message:
+      "what color do you want the text to be (enter a color keyword or a hexadecimal number):",
+  },
+  {
+    type: "list",
+    name: "shape",
+    message: "please select a shape from the following list:",
+    choices: ["\u26AA Circle", "\u25B3 Triangle", "\u25A1 Square"],
+  },
+  {
+    type: "input",
+    name: "shape color",
+    message:
+      "what color do you want the shape to be (please enter a color keyword or a hexadecimal number):",
+  },
+];
