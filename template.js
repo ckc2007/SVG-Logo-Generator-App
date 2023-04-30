4be55d57b1f5f2f531666dd74596a9de14870ec2
ckc2007 <ckc2007@gmail.com>
2023-04-30 14:38:18 -0400

    added shape switch to choose shape and color method

--- template.js
+++ template.js
@@ -3,9 +3,25 @@ const Square = require("./lib/square");
 const Triangle = require("./lib/triangle");
 
 function generateTemplate(data) {
+  let shape;
+  switch (data.shape) {
+    case "\u26AA Circle":
+      shape = new Circle();
+      break;
+    case "\u25B3 Triangle":
+      shape = new Triangle();
+      break;
+    case "\u25A1 Square":
+      shape = new Square();
+      break;
+    default:
+      throw new Error("Invalid shape selected");
+  }
   return ` <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    <${data.shape} fill="${data.shapeColor}" />
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.text}</text>
+    ${shape.getShape(data.shapeColor)}
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${
+      data.textColor
+    }">${data.text}</text>
   </svg>`;
 }
 
