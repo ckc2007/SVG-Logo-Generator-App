7f95be02b3fb28805155d5bf86998be2962f11bc
ckc2007 <ckc2007@gmail.com>
2023-04-30 14:33:47 -0400

    edited shape.js filename

--- template.js
+++ template.js
@@ -1,3 +1,6 @@
+const Circle = 
+
+
 function generateTemplate(data) {
   return ` <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
     <${data.shape} fill="${data.shapeColor}" />
